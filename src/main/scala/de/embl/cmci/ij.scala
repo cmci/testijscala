5442abcd955e3033cf417ed2dd0522b36c4b1db7
Kota@mac <miura@embl.de>
2012-11-11 11:45:31 +0100

    working now

--- src/main/scala/de/embl/cmci/ij.scala
+++ src/main/scala/de/embl/cmci/ij.scala
@@ -1,25 +1,16 @@
-package de.embl.cmci {
-  import ij._
-  import ij.plugin._
+package emblcmci.test 
 
-  object Hi { 
-    def main(args: Array[String]) {
-      println("Hi! from scala")
-      IJ.log("test from ij")
-      IJ.log(IJ.getVersion())
-      var imp = IJ.openImage("http://imagej.nih.gov/ij/images/blobs.gif")
-        imp.show()
-        IJ.wait(2000)
-        imp.close()
-    }
-  }
+import ij._
+import ij.plugin._
 
-  object Hallo {
-    def aline = IJ.log("scala test plugin")
-  }
-  class Hi_ extends PlugIn {
-    def run(arg: String) = {
-      Hallo.aline 
-    }
+object Hallo {
+  def aline = IJ.log("using Hallo object")
+}
+class HiScala extends PlugIn {
+  def run(arg: String) = {
+    Hallo.aline 
+    IJ.log("test from maven scala")
+    IJ.log("another line")
   }
 }
+
